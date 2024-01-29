7121ff8960f184a0c8e09343208f2e0b45a215d8
Hadar Bitan <hadarbitan92@gmail.com>
2024-01-29 17:47:58 +0200

    first upload

---
+++ Assets/Scripts/startOverOnTrigger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/**
+ * This component destroys its object whenever it triggers a 2D collider with the given tag.
+ */
+public class startOverOnTrigger : MonoBehaviour {
+    [Tooltip("Every object tagged with this tag will trigger game over")]
+    [SerializeField] string triggeringTag;
+    [SerializeField] string sceneName;
+
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == triggeringTag && enabled) {
+            Debug.Log("Game over!");
+            other.transform.position = Vector3.zero;
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+}
